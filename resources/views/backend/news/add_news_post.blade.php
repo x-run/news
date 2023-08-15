1a6ab7cffb7ae0f7018211e0729f619202c22de0
x-run <98542291+x-run@users.noreply.github.com>
2023-08-15 20:42:51 +0900

    .

--- resources/views/backend/news/add_news_post.blade.php
+++ resources/views/backend/news/add_news_post.blade.php
@@ -32,8 +32,8 @@
                         <div class="row">
                             <div class="form-group col-md-6 mb-3">
                                 <label for="inputEmail4" class="form-label">Category Name</label>
-                                <select name="category_id" class="form-select" id="example-select">
-                                    <option>Select Category</option>
+                                <select name="category_id" class="form-select" id="example-select" required>
+                                    <option disabled selected>Select Category</option>
                                     @foreach($categories as $category)
                                     <option value="{{ $category->id}}">{{ $category->category_name}}</option>
                                     @endforeach
@@ -55,8 +55,8 @@
                         <div class="row">
                             <div class="form-group col-md-6 mb-3">
                                 <label for="inputEmail4" class="form-label">Writer </label>
-                                <select name="user_id" class="form-select" id="example-select">
-                                    <option>Select Writer</option>
+                                <select name="user_id" class="form-select" id="example-select" required>
+                                    <option disabled selected>Select Writer</option>
                                     @foreach($adminuser as $user)
                                     <option value="{{ $user->id}}">{{ $user->name}}</option>
                                     @endforeach
@@ -74,7 +74,7 @@
                         <div class="row">
                             <div class="form-group col-md-6 mb-3">
                             <label for="example-fileinput" class="form-label">News Post Photo</label>
-                            <input type="file" name="image" id="image" class="form-control">
+                            <input type="file" name="image" id="image" class="form-control" required>
                             </div>
                         </div>
 
@@ -210,7 +210,60 @@
             });
         });
         
- </script>
+</script>
+
+<!-- Check if tiny cloud is empty -->
+
+<script type="text/javascript">
+    $(document).ready(function (){
+        $('#myForm').validate({
+            rules: {
+                news_title: {
+                    required : true,
+                }, 
+            },
+            messages :{
+                news_title: {
+                    required : 'Please Enter News Title',
+                },
+            },
+            errorElement : 'span', 
+            errorPlacement: function (error,element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight : function(element, errorClass, validClass){
+                $(element).addClass('is-invalid');
+            },
+            unhighlight : function(element, errorClass, validClass){
+                $(element).removeClass('is-invalid');
+            },
+        });
+
+        // フォームの送信ボタンのクリックイベントにリスナーを追加
+        $('button[type="submit"]').on('click', function(event) {
+            // textarea要素を取得
+            const textarea = document.querySelector('textarea[name="news_details"]');
+
+            // textarea要素が空の場合
+            if (textarea.value.trim() === '') {
+                // アラートを表示
+                alert('News Details is required.');
+
+                // フォームの送信を中断
+                event.preventDefault();
+            }
+        });
+
+    });
+    
+</script>
+
+
+
+
+
+
 
 
 @endsection
