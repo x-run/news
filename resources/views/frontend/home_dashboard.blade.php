91d36fc57b7c9ad703116bf27c35e5baaa1e76b6
x-run <98542291+x-run@users.noreply.github.com>
2023-08-21 09:03:21 +0900

    .

--- resources/views/frontend/home_dashboard.blade.php
+++ resources/views/frontend/home_dashboard.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
  
-<html lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+<html lang="ja"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
 
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,7 +11,7 @@
  
   
 <meta name="keywords" content="wun wunmedia media 沖縄 ウチナーンチュ 沖縄 online newspaper, bangla news, news, bangla, newsportal">
-<meta name="description" content="Popular News Paper of Bangladesh">
+<meta name="description" content="今日も沖縄は元気です">
 
  <link rel="stylesheet" href="{{ asset('frontend/assets/css/line-awesome.min.css')}}" />
  <link rel="stylesheet" href="{{ asset('frontend/assets/css/headstyle.css')}}" />
