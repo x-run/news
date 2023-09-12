db8019ab5dded76feebed82f601431399aeefce8
x-run <98542291+x-run@users.noreply.github.com>
2023-09-12 15:51:26 +0900

    .

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -57,7 +57,7 @@
                         </div>
                         <div class="col-6">
                             <div class="text-end">
-                                <h3 class="text-dark mt-1">👁️<span data-plugin="counterup">{{ $totalRevenue}}</span></h3>
+                                <h3 class="text-dark mt-1">👁️<span data-plugin="counterup"></span></h3>
                                 <p class="text-muted mb-1 text-truncate ">Total View</p>
                             </div>
                         </div>
