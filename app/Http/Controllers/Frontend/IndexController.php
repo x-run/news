481191c725e37ee8ca2948e57c0a9f39b0f953ba
x-run <98542291+x-run@users.noreply.github.com>
2023-10-15 21:38:34 +0900

    .

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -38,7 +38,7 @@ class IndexController extends Controller
         $skip_cat_5 = Category::skip(5)->first();
         $skip_news_5 = NewsPost::where('status',1)->where('category_id',$skip_cat_5->id)->orderBy('id','DESC') ->inRandomOrder()->limit(5)->get();
 
-        return view('frontend.index',compact('newnewspost','newspopular','skip_cat_0','skip_news_0','skip_cat_1','skip_news_1','skip_cat_2','skip_news_2','skip_cat_3','skip_news_3'));
+        return view('frontend.index',compact('newnewspost','newspopular','skip_cat_0','skip_news_0','skip_cat_1','skip_news_1','skip_cat_2','skip_news_2','skip_cat_3','skip_news_3','skip_cat_4','skip_news_4','skip_cat_5','skip_news_5'));
     }//End Method
 
     public function NewsDetails($id,$slug){
